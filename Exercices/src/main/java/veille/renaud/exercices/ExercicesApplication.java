f540f099283a13ac89c3a66a9e2466b8e9930f42
Renaud Vincent <v.pisano35@gmail.com>
2021-08-28 14:12:24 -0400

    Creation projet Exercices

---
+++ Exercices/src/main/java/veille/renaud/exercices/ExercicesApplication.java
@@ -0,0 +1,13 @@
+package veille.renaud.exercices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercicesApplication.class, args);
+    }
+
+}
---
+++ Exercices/src/test/java/veille/renaud/exercices/ExercicesApplicationTests.java
@@ -0,0 +1,13 @@
+package veille.renaud.exercices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
