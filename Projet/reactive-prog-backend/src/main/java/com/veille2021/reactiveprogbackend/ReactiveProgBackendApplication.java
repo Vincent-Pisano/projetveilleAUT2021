d4defdae4613368414fc6f8f7d22fbdd91c32059
Renaud Vincent <v.pisano35@gmail.com>
2021-09-02 13:09:52 -0400

    Creation Projet

---
+++ Projet/reactive-prog-backend/src/main/java/com/veille2021/reactiveprogbackend/ReactiveProgBackendApplication.java
@@ -0,0 +1,13 @@
+package com.veille2021.reactiveprogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgBackendApplication.class, args);
+    }
+
+}
