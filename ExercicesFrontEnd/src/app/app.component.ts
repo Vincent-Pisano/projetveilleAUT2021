e7e6a2cfddb2d6f51c110121e294a494e53ae4a6
Renaud Vincent <v.pisano35@gmail.com>
2021-09-02 11:42:23 -0400

    Test connection Event Stream depuis Spring à Angular

--- ExercicesFrontEnd/src/app/app.component.ts
+++ ExercicesFrontEnd/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { environment as env } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExercicesFrontEnd';
+
+  endSearch: boolean = false;
+  showProgressBar:boolean = false;
+  results: string[] = [];
+  resultsObserver: Observable<String[]> = new Observable<String[]>();
+
+  constructor(private ngZone: NgZone) {
+
+  }
+
+  search(event: any) {
+    event.preventDefault();
+    this.showProgressBar = true;
+    this.endSearch = false;
+    this.results = [];
+    this.resultsObserver = this.createEventSourceObserver();
+  }
+
+  createEventSourceObserver(): Observable<string[]> {
+    return new Observable<string[]>((observer: Observer<string[]>) => {
+      const source = new EventSource(env.apiUrl)
+
+      source.onmessage = (event) => {
+        console.log(event.data);
+        this.results.push(event.data);
+        this.results.sort();
+        //On utilise NgZone pour notifier 'results' des updates à l'opération async
+        //En gros = on render de nouveau l'HTML
+        //le ... | async permet de se subscribe à l'observer
+        this.ngZone.run(() => observer.next(this.results))
+      };
+
+      //quand ça se finit
+      source.onopen = (event) => {
+        if (this.endSearch) {
+          source.close();
+          this.ngZone.run(() => {
+            observer.complete();
+            this.showProgressBar = false;
+          });
+        }
+        this.endSearch = true
+      };
+    })
+  }
 }
